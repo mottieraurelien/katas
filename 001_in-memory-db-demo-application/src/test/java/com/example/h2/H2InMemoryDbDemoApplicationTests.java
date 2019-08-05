1abe8bedf4182bcf27a0bf170b3252335d7b0200
Aurélien Mottier <mottier.aurelien@gmail.com>
2019-08-05 20:01:16 +0200

    Migration to OpenJDK 12.0.2 : module 001.

--- 001_in-memory-db-demo-application/src/test/java/com/example/h2/H2InMemoryDbDemoApplicationTests.java
+++ 001_in-memory-db-demo-application/src/test/java/com/example/h2/H2InMemoryDbDemoApplicationTests.java
@@ -2,11 +2,11 @@ package com.example.h2;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@DataJpaTest
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class H2InMemoryDbDemoApplicationTests {
 
     @Test
@@ -14,3 +14,4 @@ public class H2InMemoryDbDemoApplicationTests {
     }
 
 }
+
